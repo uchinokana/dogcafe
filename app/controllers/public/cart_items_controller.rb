a881091bc79ffe8880e0c43b318d18655f2c8a00
EC2 Default User <ec2-user@ip-172-31-40-4.ap-northeast-1.compute.internal>
2022-12-20 07:26:32 +0000

    sessions

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,16 +1,59 @@
 class Public::CartItemsController < ApplicationController
-  def index
-  end
+  before_action :authenticate_customer!
 
-  def update
-  end
+    # カート商品一覧を表示
+    def index
+        @cart_items = current_customer.cart_items
+        @total_payment = @cart_items.sum{|cart_item|cart_item.item.price * cart_item.quantity * 1.1}
+        # sumメソッド：合計金額を出す
+        # 1行目の@cart_itemsにsumメソッドを用いて{}の||ブロック変数にcart_itemを代入している。(each do || end の文章と同じイメージ)
+        # cart_item.item.price：アソシエーションしているのでドットでつなげる。
+        # 『このcart_itemのitemのprice』 → 『このカート商品の商品（単体）の税抜き価格』
+        # cart_item.quantity：『このカート商品の個数』
+    end
 
-  def destroy
-  end
+    # カート商品を追加する
+    def create
+        # @cart_item = current_customer.cart_items.build(item_id: params[:item_id])
+        @cart_item = CartItem.new(cart_item_params)
+        @cart_item.customer_id = current_customer.id
+        @cart_item.item_id = params[:item_id]
+        # byebug
 
-  def all_destroy
-  end
+        if @cart_item.save
+           redirect_to customers_cart_items_path
+        else
+            render "customers/items/show"
+        end
+    end
 
-  def create
-  end
+    # 削除や個数を変更した際、カート商品を再計算する
+    def update
+        @cart_item = CartItem.find(params[:id])
+        @cart_item.update(cart_item_params)
+        redirect_to customers_cart_items_path
+    end
+
+    # カート商品を一つのみ削除
+    def destroy
+        @cart_item = CartItem.find(params[:id])
+        @cart_item.destroy
+        flash.now[:alert] = "#{@cart_item.item.name}を削除しました"
+        redirect_to customers_cart_items_path
+    end
+
+    # カート商品を空ににする
+    def all_destroy
+        @cart_item = current_customer.cart_items
+        @cart_item.destroy_all
+        flash[:alert] = "カートの商品を全て削除しました"
+        redirect_to customers_cart_items_path
+    end
+
+    private
+
+      def cart_item_params
+        params.require(:cart_item).permit(:quantity, :item_id, :customer_id)
+      end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
-  has_one_attached :image
+  has_many :cart_items
+  has_many :order_details
+
+  attachment :image
+  validates :name, {presence: true}
+  validates :description, {presence: true}
+  validates :price_without_tax, {presence: true}
 end
