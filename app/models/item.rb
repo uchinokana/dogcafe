b66f54c3878b23fe06e527e75e0d80c671b1d4c0
gowasu-hydrangea <ajisai6liked@gmail.com>
2022-12-23 05:13:50 +0000

    Fix admin items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,10 @@
 class Item < ApplicationRecord
   has_many :cart_items
   has_many :order_details
+  belongs_to :genre
 
-
+# 画像投稿
+  has_one_attached :image
 
   validates :name, {presence: true}
   validates :introduction, {presence: true}
