309d6ffcb2f43495d4db00e310de48660f60ef71
gowasu-hydrangea <ajisai6liked@gmail.com>
2022-12-23 09:03:06 +0000

    Fix public_items_index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,9 +2,15 @@ class Public::ItemsController < ApplicationController
   
   def index
     @items = Item.all
+    @quantity = Item.count
+    
   end
 
   def show
   end
   
+  # private
+  # def item_params
+  #   params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active )
+  # end
 end
