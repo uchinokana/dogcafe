4671e98ec129427e4047b0fc1dd2c69e8c2c5557
Kouya96 <115438322+Kouya96@users.noreply.github.com>
2022-12-25 04:50:50 +0000

    addressesのテーブル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_18_084006) do
+ActiveRecord::Schema.define(version: 2022_12_25_043432) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2022_12_18_084006) do
     t.string "address", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "customer_id"
   end
 
   create_table "admins", force: :cascade do |t|
