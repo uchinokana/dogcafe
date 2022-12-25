d77959ade000e364915ad693811c4b9789c7d4e8
EC2 Default User <ec2-user@ip-172-31-26-84.ap-northeast-1.compute.internal>
2022-12-25 06:41:19 +0000

    admin customers edit

--- db/migrate/20221218073117_create_addresses.rb
+++ db/migrate/20221218073117_create_addresses.rb
@@ -2,7 +2,6 @@ class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :addresses do |t|
 
-      t.integer :customer_id
       t.string :name, null: false
       t.string :postal_code, null: false
       t.string :address, null: false
